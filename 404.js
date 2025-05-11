f933cce7436c556e4565b45a71e87d2d4a307f45
Cassius W <admin@mangopi.xyz>
2025-05-11 16:38:20 +0100

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,188 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
   // Get references to DOM elements
   var terminalContainer = document.getElementById('terminal');
   var terminalText = document.getElementById('terminal-text');
+  var videoBackground = document.getElementById('myVideo');
+  var audioBackground = document.getElementById('myAudio');
   var blurredBox = document.getElementById('blurred-box');
   var closeButton = document.getElementById('close-button');
-  console.log('Credit to 3kh0 http://github.com/3kh0 for the background code')
+
   // Initial terminal text content
   var terminalTextContent = [
-      "User: unknown",
-      "IP: Loading...",
-      "System: Loading...", // System information placeholder
-      "Alt F4 pressed",
-      "Pushing a stupid rock up a hill...",
-      "Taking a mindfulness minute",
-      "Testing your patience...",
+      "404",
+      "This page could not be found",
+      "Try checking the British Museum", // System information placeholder
+      "If it hasn't been taken there then:",
+      "Try donating to Amnesty International,",
+      "Clearing your browser Cache,",
+      "And if all else fails,",
       "Press Enter To Continue",
-      
+
   ];
   var currentIndex = 0;
 
+  // Pause background video and audio
+  videoBackground.pause();
+  audioBackground.pause();
 
   // Function to type out terminal text
   function typeWriter() {
-      var line = currentIndex === 0 ? getAsciiArt() : terminalTextContent[currentIndex - 1];
-      var i = 0;
-
-      function typeChar() {
-          if (i < line.length) {
-              terminalText.textContent += line.charAt(i);
-              i++;
-              setTimeout(typeChar, 50);
-          } else {
-              terminalText.textContent += "\n";
-              currentIndex++;
-              if (currentIndex < terminalTextContent.length + 1) {
-                  typeWriter();
-              } else {
-                  addEventListeners(); // Add event listeners when typing is done
-              }
-          }
-      }
-
-      typeChar();
-  }
-
-  // Handle key press event or touch event
+@@ -53,8 +48,7 @@
   function handleInput() {
       // Hide terminal, play background video and audio, and show blurred box
       terminalContainer.style.display = 'none';
-      
+      videoBackground.play();
+      audioBackground.play();
       blurredBox.style.display = 'block';
       removeEventListeners(); // Remove event listeners after handling input
   }
-
-  // Add event listeners for both key press and touch events
-  function addEventListeners() {
-      document.addEventListener('keydown', handleKeyPress);
-      terminalContainer.addEventListener('click', handleInput); // For touch support
-  }
-
-  // Remove event listeners
-  function removeEventListeners() {
-      document.removeEventListener('keydown', handleKeyPress);
-      terminalContainer.removeEventListener('click', handleInput); // For touch support
-  }
-
-  // Handle key press event
-  function handleKeyPress(event) {
-      if (event.key === 'Enter') {
-          handleInput();
-      }
-  }
-
-  // Handle close button click event
-  closeButton.addEventListener('click', function() {
-      handleInput();
-  });
-
-  // Fetch IP address using API
-  fetch('https://api.ipify.org?format=json')
+@@ -88,12 +82,12 @@
       .then(response => response.json())
       .then(data => {
           var ipAddress = data.ip;
-          terminalTextContent[1] = "IP: " + ipAddress;
+          //terminalTextContent[1] = "IP: " + ipAddress;
           typeWriter();
       })
       .catch(error => {
           console.error('Error fetching IP address:', error);
-          terminalTextContent[1] = "IP: Unable to fetch IP address";
+          //terminalTextContent[1] = "IP: Unable to fetch IP address";
           typeWriter();
       });
 
-  // Extract system information from user agent
-  var userAgent = navigator.userAgent;
-  var systemInfo;
-  
-  // Function to get the operating system name based on user agent
-  function getOperatingSystem() {
-      if (userAgent.match(/Windows/)) {
-          // Windows OS detected
-          return getWindowsVersion();
-      } else if (userAgent.match(/Macintosh/)) {
-          // macOS detected
-          return getMacOSVersion();
-      } else if (userAgent.match(/Linux/)) {
-          // Linux OS detected
-          return "Linux";
-      } else if (userAgent.match(/Android/)) {
-          // Android OS detected
-          return getAndroidVersion();
-      } else if (userAgent.match(/iPhone|iPad|iPod/)) {
-          // iOS detected
-          return getiOSVersion();
-      } else {
-          // Default to "Unknown" if system information cannot be determined
-          return "Unknown";
-      }
-  }
-  
-  // Function to map Windows version numbers to their corresponding releases
-  function getWindowsVersion() {
-      var version = userAgent.match(/Windows NT ([\d.]+)/);
-      if (version) {
-          version = version[1];
-          switch (version) {
-              case "5.1":
-                  return "Windows XP";
-              case "6.0":
-                  return "Windows Vista";
-              case "6.1":
-                  return "Windows 7";
-              case "6.2":
-                  return "Windows 8";
-              case "6.3":
-                  return "Windows 8.1";
-              case "10.0":
-                  return "Windows 10";
-              case "10.0":
-                  return "Windows 11";
-              default:
-                  return "Windows";
-          }
-      } else {
-          return "Windows";
-      }
-  }
-  
-  // Function to get the macOS version
-  function getMacOSVersion() {
-      var version = userAgent.match(/Mac OS X ([\d_]+)/);
-      if (version) {
-          // Replace underscores with dots for macOS version
-          version = version[1].replace(/_/g, '.');
-          return "macOS " + version;
-      } else {
-          return "macOS";
-      }
-  }
-  
-  // Function to get the Android version
-  function getAndroidVersion() {
-      var version = userAgent.match(/Android ([\d.]+)/);
-      if (version) {
-          return "Android " + version[1];
-      } else {
-          return "Android";
-      }
-  }
-  
-  // Function to get the iOS version
-  function getiOSVersion() {
-      var version = userAgent.match(/OS ([\d_]+)/);
-      if (version) {
-          // Replace underscores with dots for iOS version
-          version = version[1].replace(/_/g, '.');
-          return "iOS " + version;
-      } else {
-          return "iOS";
-      }
-  }
-  
+@@ -188,48 +182,48 @@
+
   // Get the operating system information
   var operatingSystem = getOperatingSystem();
-  terminalTextContent[2] = "System: " + operatingSystem;
+  document.getElementById("user-description").innerHTML = 'Send me an <a href="mailto:cassius@mangopi.xyz" style="color:white;text-decoration:none;"><strong>email</strong></a> with this info:<br> <p style="font-size:7px">System: ' + operatingSystem + '<br>Url:' + window.location.href + '</p>';
 
   // Center the terminal window on the screen
   function centerTerminal() {
